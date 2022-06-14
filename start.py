9289f37c829c58fe68c5eec9b03f2733d8299541
rain <102387043+senseIess@users.noreply.github.com>
2022-06-13 23:54:23 -0700

    Update start.py

--- start.py
+++ start.py
@@ -19,103 +19,27 @@ ____    __    ____  ___       __       __      .______      ___      .______    
 """)
 
 print(Fore.LIGHTYELLOW_EX +"""
-OPTIONS:
+= OPTIONS:
 
-[1] CARS WALLPAPERS
-[2] CLEAN WALLPAPERS
-[3] ANIME WALLPAPERS
-[4] RANDOM WALLPAPERS
-[5] RANDOM WALLPAPERS
-
+- ANIME
+- CLEAN
+- CARS
+- RANDOM
 """)
 
-option = input(Fore.LIGHTRED_EX +"option => ")
+option = input(Fore.LIGHTRED_EX +"option name => ")
 hookurl = input(Fore.LIGHTRED_EX +"webhook url => ")
-
 webhook = f"{hookurl}"
 
+while True:
+    url = requests.get(f"https://wallhaven.cc/api/v1/search?q={option}&resolutions=1920x1080&sorting=random")
+    text = url.text
+    data = json.loads(text)
+    hook = {
+        "username" : "wallpaper generator",
+        "content" : data['data'][0]['path']
+    }
+    print(Fore.LIGHTGREEN_EX +"[!] GENERATING... PLEASE WAIT!")
+    time.sleep(5)
+    result = requests.post(webhook, json = hook)
 
-if option == "1" :
-    while True:
-
-        url = requests.get("https://wallhaven.cc/api/v1/search?q=cars&resolutions=1920x1080&sorting=random")
-
-        text = url.text
-
-        data = json.loads(text)
-
-        hook = {
-            "username" : "wallpaper generator",
-            "content" : data['data'][0]['path']
-        }
-        print(Fore.LIGHTGREEN_EX +"[!] GENERATING... PLEASE WAIT!")
-        time.sleep(5.0)
-        result = requests.post(webhook, json = hook)
-
-if option == "2" :
-    while True:
-
-        url = requests.get("https://wallhaven.cc/api/v1/search?q=clean&resolutions=1920x1080&sorting=random")
-
-        text = url.text
-
-        data = json.loads(text)
-
-        hook = {
-            "username" : "wallpaper generator",
-            "content" : data['data'][0]['path']
-        }
-        print(Fore.LIGHTGREEN_EX +"[!] GENERATING... PLEASE WAIT!")
-        time.sleep(5.0)
-        result = requests.post(webhook, json = hook)
-
-if option == "3" :
-    while True:
-
-        url = requests.get("https://wallhaven.cc/api/v1/search?categories=010&purity=110&resolutions=1920x1080&sorting=random")
-
-        text = url.text
-
-        data = json.loads(text)
-
-        hook = {
-            "username" : "wallpaper generator",
-            "content" : data['data'][0]['path']
-        }
-        print(Fore.LIGHTGREEN_EX +"[!] GENERATING... PLEASE WAIT!")
-        time.sleep(5.0)
-        result = requests.post(webhook, json = hook)
-
-if option == "4" :
-    while True:
-
-        url = requests.get("https://tokyo.tokyocity.repl.co/wallpaper")
-
-        text = url.text
-
-        data = json.loads(text)
-
-        hook = {
-            "username" : "wallpaper generator",
-            "content" : data['wpp']
-        }
-        print(Fore.LIGHTGREEN_EX +"[!] GENERATING... PLEASE WAIT!")
-        time.sleep(5.0)
-        result = requests.post(webhook, json = hook)
-
-if option == "5" :
-    while True:
-
-        url = requests.get("https://wallhaven.cc/api/v1/search?sorting=random&resolutions=1920x1080")
-
-        text = url.text
-
-        data = json.loads(text)
-
-        hook = {
-            "username" : "wallpaper generator",
-            "content" : data['data'][0]['path']
-        }
-        print(Fore.LIGHTGREEN_EX +"[!] GENERATING... PLEASE WAIT!")
-        time.sleep(5.0)
-        result = requests.post(webhook, json = hook)  
